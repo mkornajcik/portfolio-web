d3ccde36fff2b1ef45706f29338e0d3b6caa57e9
Marko <marko.kornajcik6@gmail.com>
2024-09-08 11:54:31 +0300

    removed unused routes and updated requirements

--- main.py
+++ main.py
@@ -17,20 +17,5 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
-
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
-
-
-@app.route("/projects")
-def projects():
-    return render_template("projects.html")
-
-
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=False, port=5001)
